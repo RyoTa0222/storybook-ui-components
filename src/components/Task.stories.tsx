a1557b05265d7d59234fed0067f6139e96123f5b
Matsumoto Ryota <awpjdmga1943@icloud.com>
2021-05-05 13:57:39 +0900

    fixed

--- src/components/Task.stories.tsx
+++ src/components/Task.stories.tsx
@@ -22,12 +22,20 @@ Default.args = {
   },
 };
 
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
+export const LongTitle = Template.bind({});
+LongTitle.args = {
+  task: {
+    ...Default.args.task!,
+    title: longTitleString,
+  },
+};
+
 export const Pinned = Template.bind({});
 Pinned.args = {
   task: {
-    id: "1",
-    title: "Test Task",
-    updatedAt: new Date(2018, 0, 1, 9, 0),
+    ...Default.args.task!,
     state: "TASK_PINNED",
   },
 };
@@ -35,9 +43,7 @@ Pinned.args = {
 export const Archived = Template.bind({});
 Archived.args = {
   task: {
-    id: "1",
-    title: "Test Task",
-    updatedAt: new Date(2018, 0, 1, 9, 0),
+    ...Default.args.task!,
     state: "TASK_ARCHIVED",
   },
 };
